4581b6f36af4f05fb366846f42dab6deb4c35e02
Jakub Gwóźdź <jakub.gwozdz@icloud.com>
2018-11-10 13:38:28 +0100

    Initial Commit

---
+++ MyHomeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyHomeApp
+//
+//  Created by Jakub Gwóźdź on 10/11/2018.
+//  Copyright © 2018 Jakub Gwóźdź. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
